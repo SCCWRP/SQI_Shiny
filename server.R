158b00ecfad0acaa027032d938180877138b588b
fawda123 <mbafs2012@gmail.com>
2018-05-30 11:37:44 -0700

    first version of app done

--- server.R
+++ server.R
@@ -1,14 +1,122 @@
 library(shiny)
 library(tidyverse)
+library(randomForest)
+library(gridExtra)
+
+# load rf models
+load(file = 'data/hab.rf.wp.Rdata')
+load(file = 'data/wq.rf.wp.Rdata')
+
+# plot data
+len.x<-20
+plot.dat.chem<-expand.grid(
+  TN2=seq(from=0,to=5, length.out=len.x*2.5),
+  TP=seq(from=0,to=2, length.out=len.x*2.5),
+  Cond=seq(from=0,to=2000, length.out=len.x*2.5))
+
+plot.dat.chem$pChem<-predict(wq.rf.wp, newdata=plot.dat.chem, type="prob")[,2]
 
 # server logic
 server <- function(input, output, session) {
   
-  #
-  spat <- reactive({
+  # values to predict
+  toprd <- reactive({
+    
+    out <- data.frame(
+      CSCI = input$csci,
+      ASCI = input$asci,
+      TN2 = input$tn,
+      TP = input$tp,
+      Cond = input$cond,
+      indexscore_cram = input$cram,
+      PCT_SAFN = input$safn,
+      H_AqHab = input$aqhab,
+      H_SubNat = input$subnat,
+      Ev_FlowHab = input$flowhab,
+      XCMG = input$veg
+    )
+    
+    return(out)
     
   })
   
-
+  # categories from predictions
+  cats <- reactive({
+    
+    mydf.prediction <- toprd()
+    
+    mydf.prediction$pChem<-predict(wq.rf.wp, newdata=mydf.prediction, type="prob")[,2]
+    mydf.prediction$pHab<-predict(hab.rf.wp, newdata=mydf.prediction, type="prob")[,2]
+    mydf.prediction$pChemHab<-mydf.prediction$pChem*mydf.prediction$pHab
+    
+    mydf.prediction$BiologicalCondition<-ifelse(mydf.prediction$CSCI>=0.79 & mydf.prediction$ASCI>=60,"Healthy",
+                                                ifelse(mydf.prediction$CSCI<0.79 & mydf.prediction$ASCI<60,"Impacted for BMI and algae",
+                                                       ifelse(mydf.prediction$CSCI<0.79 & mydf.prediction$ASCI>=60,"Impacted for BMI",
+                                                              ifelse(mydf.prediction$CSCI>=0.79 & mydf.prediction$ASCI<60,"Impacted for algae", "XXXXX"
+                                                              ))))
+    mydf.prediction$WaterChemistryCondition<-ifelse(mydf.prediction$pChem>=0.67,"Low stress",ifelse(mydf.prediction$pChem>=0.33,"Moderate stress","Severe stress"))
+    mydf.prediction$HabitatCondition<-ifelse(mydf.prediction$pHab>=0.67,"Low stress",ifelse(mydf.prediction$pHab>=0.33,"Moderate stress","Severe stress"))
+    mydf.prediction$OverallStressCondition<-ifelse(mydf.prediction$pChemHab>=0.67,"Low stress",ifelse(mydf.prediction$pChemHab>=0.33,"Moderate stress","Severe stress"))
+    mydf.prediction$OverallStressCondition_detail<-ifelse(mydf.prediction$pChemHab>=0.67,"Low stress",
+                                                          ifelse(mydf.prediction$pChemHab<0.67 & mydf.prediction$pHab<0.67, "Stressed by chemistry and habitat degradation",
+                                                                 ifelse(mydf.prediction$pChemHab<0.67 & mydf.prediction$pHab>=0.67, "Stressed by chemistry degradation",
+                                                                        ifelse(mydf.prediction$pChemHab>=0.67 & mydf.prediction$pHab<0.67, "Stressed by habitat degradation",
+                                                                               ifelse(mydf.prediction$pChemHab>=0.67 & mydf.prediction$pHab>=0.67, "Stressed by low levels of chemistry or habitat degradation",
+                                                                                      "XXXXX")))))
+    
+    mydf.prediction$StreamHealthIndex<-ifelse(mydf.prediction$BiologicalCondition=="Healthy" & mydf.prediction$OverallStressCondition=="Low stress","Healthy and unstressed",
+                                              ifelse(mydf.prediction$BiologicalCondition=="Healthy" & mydf.prediction$OverallStressCondition!="Low stress","Healthy and resilient",
+                                                     ifelse(mydf.prediction$BiologicalCondition!="Healthy" & mydf.prediction$OverallStressCondition=="Low stress","Impacted by unknown stress",
+                                                            ifelse(mydf.prediction$BiologicalCondition!="Healthy" & mydf.prediction$OverallStressCondition!="Low stress","Impacted and stressed",
+                                                                   "XXXXX"))))
   
+    return(mydf.prediction)
+    
+  })
+  
+  # text output
+  output$overall <- renderText(paste0('Overall: ', cats()$StreamHealthIndex))
+  output$biolcon <- renderText(paste0('Biological condition: ', cats()$BiologicalCondition))
+  output$strscon <- renderText(paste0('Stress condition: ', cats()$OverallStressCondition))
+  output$strsdet <- renderText(paste0('Stress condition detail: ', cats()$OverallStressCondition_detail))
+
+  # plots
+  output$plos <- renderPlot({
+    
+    mydf.prediction <- cats()
+    
+    #Select the subset of rows with similar conductivity
+    closest.cond<-plot.dat.chem$Cond[which.min(abs(plot.dat.chem$Cond - mydf.prediction$Cond))]
+    plot.dat.chem_tntp<-plot.dat.chem[which(plot.dat.chem$Cond==closest.cond),]
+    closest.tn<-plot.dat.chem$TN2[which.min(abs(plot.dat.chem$TN2 - mydf.prediction$TN2))]
+    plot.dat.chem_tpcond<-plot.dat.chem[which(plot.dat.chem$TN2==closest.tn),]
+    closest.tp<-plot.dat.chem$TP[which.min(abs(plot.dat.chem$TP - mydf.prediction$TP))]
+    plot.dat.chem_tncond<-plot.dat.chem[which(plot.dat.chem$TP==closest.tp),]
+    
+    #TN-TP plot
+    my.pred.plot<-mydf.prediction
+    my.pred.plot$TN2<-pmin(max(plot.dat.chem$TN2),my.pred.plot$TN2)#*1.01
+    my.pred.plot$TP<-pmin(max(plot.dat.chem$TP),my.pred.plot$TP)#*1.01
+    my.pred.plot$Cond<-pmin(max(plot.dat.chem$Cond),my.pred.plot$Cond)#*1.01
+    
+    p1 <- ggplot(data=plot.dat.chem_tntp, aes(x=TN2, y=TP))+
+      geom_tile(aes(fill=pChem))+
+      scale_fill_gradient2(low="#d7191c", mid="#ffffbf", high="#2c7bb6", midpoint=0.5)+
+      geom_point(data=my.pred.plot, shape=21, size = 4, fill="white")+
+      theme_minimal(base_size = 14, base_family = 'serif') +
+      xlab("Total N (mg/L)")+ ylab("Total P (mg/L)")+
+      ggtitle(paste("Sp Cond at",mydf.prediction$Cond))
+    
+    p2 <- ggplot(data=plot.dat.chem_tncond, aes(x=TN2, y=Cond))+
+      geom_tile(aes(fill=pChem))+
+      scale_fill_gradient2(low="#d7191c", mid="#ffffbf", high="#2c7bb6", midpoint=0.5)+
+      geom_point(data=my.pred.plot, shape=21, size = 4, fill="white")+
+      theme_minimal(base_size = 14, base_family = 'serif') +
+      xlab("Total N (mg/L)")+ ylab("Sp Cond (uS/cm)") + 
+      ggtitle(paste("Total P at",mydf.prediction$TP))
+    
+    grid.arrange(p1, p2, ncol = 2)
+    
+  })
+    
 }
--- ui.R
+++ ui.R
@@ -1,28 +1,91 @@
-library(leaflet)
-library(shinyjs)
-library(shinyBS)
-library(shinyCustom)
-library(shinydashboard)
-library(shinyWidgets)
-library(ShinyDash)
-library(tidyverse)
-library(mapview)
-library(rvest)
-
-# watersheds to select from data folder
-shds <- list.files('data') %>%
-  str_subset('Santa Ana\\.RData$') %>% 
-  gsub('\\.RData$|^scrs_|^spat_', '', .) %>%
-  unique
-
-# column padding global
-pad <- 'padding:0px;'
+library(shiny)
 
 # Define UI for application
 shinyUI(fluidPage(
   
   theme = 'styles.css',
 
+  # bio inputs
+  fluidRow(
+    
+    column(width = 12, h3('Biology')),
+    column(width = 4, 
+        numericInput('csci', h5('CSCI score. Enter a number between 0 and ~1.4'), value = 0.45, min = 0, max = 1.5) 
+        ), 
+    column(width = 4, 
+        numericInput('asci', h5('ASCI score (H20 for now). Enter a number between 0 and 100'), value = 25, min = 0, max = 100)    
+        )
+    
+  ),
+  
+  # chem inputs
+  fluidRow(
+    
+    column(width = 12, h3('Chemistry')),
+    column(width = 4, 
+           numericInput('tn', h5('Total N (mg/L). Enter a number greater than zero'), value = 10.1, min = 0) 
+    ), 
+    column(width = 4, 
+           numericInput('tp', h5('Total P (mg/L). Enter a number greater than zero'), value = 10.001, min = 0)    
+    ), 
+    column(width = 4, 
+           numericInput('cond', h5('Specific conductance (uS/cm). Enter a number greater than zero'), value = 450, min = 0)    
+    )
+    
+  ),
+  
+  # hab inputs
+  fluidRow(
+    
+    column(width = 12, h3('Habitat')),
+    column(width = 4, 
+           numericInput('cram', h5('CRAM score. Enter a number between 0 and 100'), value = 75, min = 0, max = 100) 
+    ), 
+    column(width = 4, 
+           numericInput('safn', h5('% sands and fines. Enter a number between 0 and 100'), value = 35, min = 0, max = 100)    
+    ), 
+    column(width = 4, 
+           numericInput('aqhab', h5('SW diversity of aquatic habitats. Enter a number between 0 and ~2.5'), value = 1.5, min = 0, max = 2.6)    
+    ),
+    column(width = 4, 
+           numericInput('subnat', h5('SW diversity of streambed substrate. Enter a number between 0 and ~2.5'), value = 1.9, min = 0, max = 2.6) 
+    ), 
+    column(width = 4, 
+           numericInput('flowhab', h5('Evenness of flow habitat types (e.g., riffles, pools). Enter a number between zero and 1'), value = 0.5, min = 0, max = 1)    
+    ), 
+    column(width = 4, 
+           numericInput('veg', h5('Riparian veg cover. Enter a number from 0 to ~265'), value = 200, min = 0, max = 300)    
+    )
+    
+  ),
+  
+  # text output
+  fluidRow(
+   
+    column(width = 12, h3('Stream Health Index')), 
+    column(width = 12, 
+           textOutput('overall')
+           ), 
+    column(width = 12, 
+           textOutput('biolcon')
+           ), 
+    column(width = 12, 
+           textOutput('strscon')
+           ), 
+    column(width = 12, 
+           textOutput('strsdet')
+           )
+  ),
+  
+  # plot outputs
+  fluidRow(
+    
+    HTML('<p></p>'),
+    column(width = 12, 
+           plotOutput('plos')
+           )
+    
+  )
   
 ))
 
