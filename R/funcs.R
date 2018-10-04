247225487a40f97566916c2426ed2a9913a164a3
fawda123 <mbafs2012@gmail.com>
2018-10-03 17:22:39 -0700

    added dynamic legend to map

--- R/funcs.R
+++ R/funcs.R
@@ -1,5 +1,5 @@
 # descriptor colors for wqi
-getdsccol <- function(dscin = NULL, palout = F){
+getdsccol <- function(dscin = NULL, palout = F, palfac = NULL){
   
   if(is.factor(dscin))
     stop('dscin must be character')
@@ -11,35 +11,43 @@ getdsccol <- function(dscin = NULL, palout = F){
   l4 <- '#DC143C' #'Crimson'
   
   if(palout) return(c(l1, l2, l3, l4))
-  
+
   #  color categories
-  overallcol <- list(
+  StreamHealthIndex <- list(
     'Healthy and unstressed' = l1, 
     'Healthy and resilient' = l2,
     'Impacted by unknown stress' = l3,
     'Impacted and stressed' = l4
   )                                   
-  biologscol <- list(
+  BiologicalCondition <- list(
     'Healthy' = l1,
     'Impacted for BMI' = l2,
     'Impacted for algae' = l3,
     'Impacted for BMI and algae' = l4
   )
-  strsovrcol <- list(
+  OverallStressCondition <- list(
     'Low' = l1, 
     'Moderate' = l3,
     'Severe' = l4
   )
-  strsdetcol <- list(
+  OverallStressCondition_detail <- list(
     'Low stress' = l1, 
     'Stressed by chemistry degradation' = l2,
     'Stressed by habitat degradation' = l3,
     'Stressed by chemistry and habitat degradation' = l4, 
     'Stressed by low levels of chemistry or habitat degradation' = l1
   )
+
+  # return individual palette with label is provided
+  if(!is.null(palfac)){
+    out <- get(palfac) %>% 
+      unlist %>% 
+      data.frame(nms = names(.), col = ., stringsAsFactors = F)
+    return(out)
+  }
   
   # get all
-  allcol <- c(overallcol, biologscol, strsovrcol, strsdetcol)
+  allcol <- c(StreamHealthIndex, BiologicalCondition, OverallStressCondition, OverallStressCondition_detail)
   allcol <- unlist(allcol)
   
   # select colors
